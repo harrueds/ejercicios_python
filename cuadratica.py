bb058678ff1250660038335228274b7e53d4a155
Henzo Alejandro Arrué Muñoz <harrue.ds@gmail.com>
2025-05-29 07:19:16 -0400

    ejercicio cuadratica

---
+++ cuadratica.py
@@ -0,0 +1,13 @@
+a=float(input("coef cuad="))
+b=float(input("coef lineal="))
+c=float(input("coaf libre="))
+disc=b**2-4*a*c
+if disc == 0:
+    sol=-b/(2*a)
+    print(f"única solución:{sol}")
+elif disc < 0:
+    print("no hay solución")
+elif disc > 0:
+    sol1=(-b-disc**0.5)/(2*a)
+    sol2=(-b+disc**0.5)/(2*a)
+    print(f"dos soluciones {sol1} y {sol2}")
