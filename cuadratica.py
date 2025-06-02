166f2d2c2ce9e6d4f1c8cfff41bbaa90df7bffef
Henzo Alejandro Arrué Muñoz <harrue.ds@gmail.com>
2025-06-02 15:05:18 -0400

    02062025

--- cuadratica.py
+++ cuadratica.py
@@ -1,13 +1,16 @@
+# se ingresan los coeficientes e una ecuación cuadrática
+# y devuelve las soluciones según corresponda.
+
 a = float(input("coef cuad="))
 b = float(input("coef lineal="))
 c = float(input("coef libre="))
 disc = b**2 - 4 * a * c
 if disc == 0:
-    sol = -b / (2 * a)
-    print(f"única solución:{sol}")
+sol = -b / (2 * a)
+print(f"única solución:{sol}")
 elif disc < 0:
-    print("no hay solución")
+print("no hay solución")
 elif disc > 0:
-    sol1 = (-b - disc**0.5) / (2 * a)
-    sol2 = (-b + disc**0.5) / (2 * a)
-    print(f"dos soluciones {sol1} y {sol2}")
+sol1 = (-b - disc**0.5) / (2 * a)
+sol2 = (-b + disc**0.5) / (2 * a)
+print(f"dos soluciones {sol1} y {sol2}")
